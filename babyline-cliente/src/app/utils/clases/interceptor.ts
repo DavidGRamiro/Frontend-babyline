40ae765936522352041eda0350ed31fa1a9b8831
David Gonzalez <davidgramiro@gmail.com>
2024-11-06 17:52:29 +0100

    Delete login route in interceptor

--- babyline-cliente/src/app/utils/clases/interceptor.ts
+++ babyline-cliente/src/app/utils/clases/interceptor.ts
@@ -6,6 +6,12 @@ import { Observable } from "rxjs";
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpSentEvent | HttpHeaderResponse | HttpProgressEvent | HttpResponse<any> |HttpUserEvent<any>> {
+    
+      // Excluir la URL de login
+      if (req.url.includes('/login')) {
+        return next.handle(req);
+      }
+    
     const authToken = inject(HeaderService).getHeaders();
         const authReq = req.clone({
       headers: req.headers.set('Authorization', `Token ${authToken}`)
