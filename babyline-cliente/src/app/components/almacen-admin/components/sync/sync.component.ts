1f38f9ba0b2060d4819d5074fa1c81fb7b500dd8
David Gonzalez <davidgramiro@gmail.com>
2025-02-16 19:29:16 +0100

    Preparaci'on de componente para sincronizacion de BBDD

--- babyline-cliente/src/app/components/almacen-admin/components/sync/sync.component.ts
+++ babyline-cliente/src/app/components/almacen-admin/components/sync/sync.component.ts
@@ -1,17 +1,57 @@
 import { CommonModule } from '@angular/common';
-import { Component, type OnInit } from '@angular/core';
+import { Component, inject, type OnInit } from '@angular/core';
+import { PrimeNgModule } from '../../../../utils/primeNG/primeNg.module';
+import { Message } from 'primeng/api';
+import { FormsModule } from '@angular/forms';
+import { SyncService } from '../../services/sync.service';
 
 @Component({
     selector: 'app-sync',
     standalone: true,
     imports: [
-        CommonModule,
+        CommonModule, PrimeNgModule, FormsModule
     ],
     templateUrl: './sync.component.html',
     styleUrl: './sync.component.css',
 })
 export class SyncComponent implements OnInit {
 
-    ngOnInit(): void { }
+
+  //Servicios
+  private _syncService  = inject(SyncService)
+
+  // Mensajes de informacion
+  public messages_info: Message[] = [];
+  public messages_info_2: Message[] = [];
+
+  // Construcotr de SelectBox
+  public BBDDSeleccionada : any = null
+  public BBDD : any[] = [
+    {code: 1,  name: 'BBDD 1'},
+    {code: 2,  name: 'BBDD 2'},
+    {code: 3,  name: 'Todas'},
+  ]
+  // Variable control de estado
+  public loading : boolean = false;
+
+
+  ngOnInit() {
+      this.messages_info = [{ severity: 'info', detail: 'Panel de sincronizacion de base de datos.' }];
+      this.messages_info_2 = [{ severity: 'contrast', detail: 'Este proceso se lanzara automaticamente en cada actualizacion del producto, pero puedes hacerlo manualmente desde esta ventana.' }];
+  }
+
+  syncBBDD(){
+    this.loading = true
+    console.log('Sincronizando')
+    this._syncService.sincronizar().subscribe({
+      next :( data:any) => {
+        console.log(data)
+      },
+      error : (err: any) => {
+        console.log(err)
+      }
+    })
+  
+  }
 
 }
---
+++ babyline-cliente/src/app/components/almacen-admin/services/sync.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SyncService {
+
+  constructor(private _http : HttpClient) { }
+
+  private url = 'http://localhost:8000/sync';
+
+  sincronizar(){
+    return this._http.get(this.url)
+  }
+
+}
